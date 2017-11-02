acf5c537049bb67835c66f44dfadaaba93cb8b87
Kacper <kacper.krycki@gmail.com>
2017-11-02 01:02:57 +0100

    Folds exercises

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -26,6 +26,11 @@ object List {
     case Cons(_, xs) => xs
   }
 
+  def head[A](as: List[A]): A = as match {
+    case Nil => throw new RuntimeException("empty list")
+    case Cons(x, _) => x
+  }
+
   def setHead[A](as: List[A], h: A) =
     Cons(h, List.tail(as))
 
@@ -49,6 +54,52 @@ object List {
     case _ => Nil
   }
 
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0)((x,y) => x + y)
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0)(_ * _)
+
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((_, y) => y + 1)
+
+  def sum3(ns: List[Int]): Int =
+    foldLeft(ns, 0)((x,y) => x + y)
+
+  def product3(ns: List[Double]): Double =
+    foldLeft(ns, 1.0)(_ * _)
+
+  def length2[A](as: List[A]): Int =
+    foldLeft(as, 0)((x, _) => x + 1)
+
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) =>foldLeft(xs, f(z, x))(f)
+  }
+
+
+  def reverse[A](as: List[A]): List[A] =
+    foldLeft(as, Nil:List[A])((xs, h) => Cons(h, xs))
+
+  def foldLeft2[A,B](as: List[A], z: B)(f: (B, A) => B): B = {
+    foldRight(as, (b: B) => b)((a: A, g: B => B) => (b: B) => g(f(b,a)): B)(z)
+  }
+
+  def foldRight2[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(as, (b:B) => b)((g,a) => b => g(f(a,b)))(z)
+
+  def trace[A](s: String, a: A): A = {
+    println(s)
+    a
+  }
+
   def main(args: Array[String]): Unit = {
     val list = List(1, 2, 3, 4, 5)
 
@@ -66,5 +117,14 @@ object List {
 
     println(init(list) == List(1, 2, 3, 4))
     println(init(List(1)) == Nil)
+
+//    println(foldRight(List(1,2,3),  Nil:List[Int])(Cons(_,_)))
+//
+////    println(length(list) == 5)
+//
+//    println(sum2(list))
+//    println(sum3(list))
+
+    println(reverse(list))
   }
 }
