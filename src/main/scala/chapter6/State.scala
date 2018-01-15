48435d1d8f54dff497ca28bb1d9ae09d9b41c8ab
Kacper Krycki <kacper.krycki@bms.com.pl>
2018-01-15 13:13:08 +0100

    EXERCISE 6.10

--- src/main/scala/chapter6/State.scala
+++ src/main/scala/chapter6/State.scala
@@ -1,5 +1,25 @@
 package chapter6
 
-class State[S,+A](run: S => (A,S)) {
+//unit , map , map2 , flatMap , and sequence
+case class State[S,+A](run: S => (A,S)) {
 
+  def map[B](f: A => B): State[S, B] =
+    State(this.run.andThen(x => (f(x._1), x._2)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    State(this.run.andThen(xa => sb.run.andThen(xb => (f(xa._1, xb._1), xb._2))(xa._2)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(this.run.andThen(x => f(x._1).run(x._2)))
+
+
+}
+
+object State {
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(State.unit[S, List[A]](List()))(_.map2(_)(_::_))
 }
