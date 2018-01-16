b29fd5a46681844458c09332efd1237b81fda804
Kacper Krycki <kacper.krycki@gmail.com>
2018-01-16 01:13:01 +0100

    Excercise 6.10 in progress

--- src/main/scala/chapter6/Input.scala
+++ src/main/scala/chapter6/Input.scala
@@ -6,8 +6,16 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int) {
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+  def insertInput(input: Input): (Machine, (Int, Int)) =
+    (this, input) match {
+      case (Machine(_, c1, c2), _) if c1 <= 0 => (this, (c1, c2))
+      case (Machine(false, c1, c2), Coin) => (Machine(true, c1, c2 + 1), (c1, c2 + 1))
+      case (Machine(true, c1, c2), Turn) => (Machine(false, c1 - 1, c2), (c1 - 1, c2))
+      case (Machine(f, c1, c2), _) => (this, (c1, c2))
+    }
 
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+    inputs.foldRight(State.unit[Machine, (Int, Int)])((i, s) => s.)
 
     State.unit[Machine, (Int, Int)]((0, 0))
   }
--- src/main/scala/chapter6/State.scala
+++ src/main/scala/chapter6/State.scala
@@ -12,6 +12,15 @@ case class State[S,+A](run: S => (A,S)) {
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     State(this.run.andThen(x => f(x._1).run(x._2)))
 
+  def modify(f: S => S): State[S, Unit] = for {
+    s <- get // Gets the current state and assigns it to `s`.
+    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
 }
 
 object State {
