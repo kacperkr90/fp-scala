1e1b1c3741dbc21beb953016a07c04024a2ff71b
Kacper Krycki <kacper.krycki@bms.com.pl>
2018-06-26 16:56:12 +0200

    Up to 7.4

--- src/main/scala/chapter7/Par.scala
+++ src/main/scala/chapter7/Par.scala
@@ -1,6 +1,6 @@
 package chapter7
 
-import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+import java.util.concurrent._
 
 
 object Par {
@@ -34,6 +34,10 @@ object Par {
   def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 
   def map[A, B](a: Par[A])(f: A => B): Par[B] = map2(a, unit())((aa, _) => f(aa))
+  def mapViaFlatMap[A, B](a: Par[A])(f: A => B): Par[B] = flatMap(a)(aa => lazyUnit(f(aa)))
+
+  def flatMap[A, B](a: Par[A])(f: A => Par[B]): Par[B] =
+    es => f(a(es).get)(es)
 
   def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
 
@@ -46,9 +50,40 @@ object Par {
   }
 
   def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
-    val fs = parMap(as)(a => if (f(a)) List(a) else List(a))
+    val fs = parMap(as)(a => if (f(a)) List(a) else List())
     map(fs)(_.flatten)
   }
 
+  def foldAsync[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B)(g: (B, B) => B): Par[B] =
+    if (as.length <= 1)
+      unit(as.headOption.map(a => f(a, z)) getOrElse z)
+    else {
+      val (l,r) = as.splitAt(as.length/2)
+      map2(fork(foldAsync(l)(z)(f)(g)), fork(foldAsync(r)(z)(f)(g)))(g(_,_))
+    }
 
+  def sum(ints: IndexedSeq[Int]): Par[Int] =
+    foldAsync(ints)(0)(_ + _)(_ + _)
+
+  def max(ints: IndexedSeq[Int]): Par[Option[Int]] = {
+    def optionMax = (a: Int, b: Option[Int]) =>
+      b.map(bb => Option(Math.max(a, bb)))
+        .getOrElse(Option(a))
+
+    foldAsync(ints)(Option.empty:Option[Int])(optionMax)((z1, z2) => z1.flatMap(z => optionMax(z, z2)))
+  }
+
+  def totalWords(ps: List[String]): Par[Int] = {
+    val s = ps.map(asyncF(_.split(" "))).map(map(_)(_.length))
+    flatMap(sequence(s))(ws => sum(ws.toIndexedSeq))
+  }
+
+  def map3[A, B, C, D](a: Par[A], b: Par[B], c: Par[C])(f: (A, B, C) => D): Par[D] =
+    map2(a, map2(b, c)((bb, cc) => (aa: A) => f(aa, bb, cc)))((aa, g) => g(aa))
+
+  def map4[A, B, C, D, E](a: Par[A], b: Par[B], c: Par[C], d: Par[D])(f: (A, B, C, D) => E): Par[E] =
+    map2(a, map3(b, c, d)((bb, cc, dd) => (aa: A) => f(aa, bb, cc, dd)))((aa, g) => g(aa))
+
+  def map5[A, B, C, D, E, F](a: Par[A], b: Par[B], c: Par[C], d: Par[D], e: Par[E])(f: (A, B, C, D, E) => F): Par[F] =
+    map2(a, map4(b, c, d, e)((bb, cc, dd, ee) => (aa: A) => f(aa, bb, cc, dd, ee)))((aa, g) => g(aa))
 }
--- src/test/scala/chapter7/ParTest.scala
+++ src/test/scala/chapter7/ParTest.scala
@@ -1,14 +1,27 @@
 package chapter7
 
-import java.util.concurrent.Executors
+import java.util.concurrent.ForkJoinPool
 
 import org.scalatest.{FlatSpec, Matchers}
 
 class ParTest extends FlatSpec with Matchers {
 
-  val es = Executors.newSingleThreadExecutor()
+  val es = ForkJoinPool.commonPool()
 
   Par.sequence(List(Par.unit(1), Par.unit(2), Par.unit(3)))(es).get should be (Par.unit(List(1, 2, 3))(es).get)
 
   Par.parFilter(List(1, 2, 3, 4, 5, 6))(_ % 2 == 0)(es).get should be (List(2, 4, 6))
+
+  Par.sum(IndexedSeq(1, 2, 3, 4, 5, 6))(es).get should be (21)
+
+  Par.max(IndexedSeq(1, 2, 3, 6, 5, 4))(es).get should be (Option(6))
+  Par.max(IndexedSeq(1, 2, 3, 6, 5, 4, 10))(es).get should be (Option(10))
+  Par.max(IndexedSeq())(es).get should be (Option.empty)
+
+  Par.totalWords(List())(es).get should be (0)
+  Par.totalWords(List("The Grand Hotel Budapest", "Alien Requiem"))(es).get should be (6)
+
+  Par.map3(Par.unit(1), Par.unit(2), Par.unit(3))(_ + _ + _)(es).get should be (6)
+  Par.map4(Par.unit(1), Par.unit(2), Par.unit(3), Par.unit(4))(_ + _ + _ + _)(es).get should be (10)
+  Par.map5(Par.unit(1), Par.unit(2), Par.unit(3), Par.unit(4), Par.unit(5))(_ + _ + _ + _ + _)(es).get should be (15)
 }
