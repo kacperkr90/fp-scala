6328d00cee6755ea65a8e7e7b374a63a39937c28
Kacper Krycki <kacper.krycki@bms.com.pl>
2018-07-05 17:45:14 +0200

    EXERCISE 8.2

--- src/test/scala/chapter8/Ch8Test.scala
+++ src/test/scala/chapter8/Ch8Test.scala
@@ -1,7 +1,7 @@
 package chapter8
 
 import chapter8.Ch8.sum
-import org.scalacheck.{Gen, Properties}
+import org.scalacheck.{Gen, Prop, Properties}
 import org.scalacheck.Prop.forAll
 
 class Ch8Test extends Properties("Practicing properties") {
@@ -11,5 +11,10 @@ class Ch8Test extends Properties("Practicing properties") {
 
   val fives: Gen[List[Int]] = Gen.listOf(Gen.const(5))
   property("same value list sum should equals to value multiplied by list lenght") = forAll(fives)(ns => sum(ns) == ns.size * 5)
+
+  val list1: Gen[List[Int]] = Gen.nonEmptyListOf(Gen.const(5))
+  property("should find max number in non-empty list") = forAll(list1)(ns => ns.max == 5)
+
+  property("should throw exception on empty lists") = forAll(list)(ns => ns.max == Prop.exception)
 
 }
