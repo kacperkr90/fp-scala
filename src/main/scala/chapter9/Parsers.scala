5e63a453fb31f909cbc424be449408de80b891b9
Kacper Krycki <kacper.krycki@gmail.com>
2018-10-03 22:09:01 +0200

    Chapter 9 Intro #2

--- src/main/scala/chapter9/Parsers.scala
+++ src/main/scala/chapter9/Parsers.scala
@@ -5,10 +5,22 @@ trait Parsers[ParseError, Parser[+_]] {self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
   def char(c: Char): Parser[Char]
   def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  run(or(string("abra"),string("cadabra")))("abra") == Right("abra")
+  run(or(string("abra"),string("cadabra")))("cadabra") == Right("cadabra")
+
   implicit def string(s: String): Parser[String]
+
+  run(string("abracadabra"))("abracadabra") == Right("abracadabra")
+
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
-  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
-    ParserOps(f(a))
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
+  run(listOfN(3, "ab" | "cad"))("ababcad") == Right("ababcad")
+  run(listOfN(3, "ab" | "cad"))("cadabab") == Right("cadabab")
+  run(listOfN(3, "ab" | "cad"))("ababab") == Right("ababab")
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
